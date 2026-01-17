959d5176fbac48255cd69849c2e37e9ddaedeb56
Ayush Mehta <ayushmehtabps@gmail.com>
2026-01-17 18:12:10 +0530

    chat room and chat request models added

---
+++ backend/devLink-server/models/chat_model.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+
+type ChatRequest struct{
+	ID bson.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	SenderID bson.ObjectID `bson:"sender_id" json:"sender_id"`
+	ReceiverID bson.ObjectID `bson:"receiver_id" json:"receiver_id"`
+	Status string `bson:"status" json:"status"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+}
+
+
+type ChatRoom struct{
+	ID bson.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Participants []bson.ObjectID `bson:"participants" json:"participants"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	
+}
+
+
+
+
+
+
