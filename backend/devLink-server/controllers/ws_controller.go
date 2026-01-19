0d0332dd8b82085d3aa5c6d7bbe4e89a1d1de5b2
Ayush Mehta <ayushmehtabps@gmail.com>
2026-01-19 21:46:11 +0530

    jwt check

--- backend/devLink-server/controllers/ws_controller.go
+++ backend/devLink-server/controllers/ws_controller.go
@@ -2,8 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 
@@ -14,4 +19,33 @@ var upgarder=websocket.Upgrader{
 	},
 }
 
+func ChatWebSocket(client *mongo.Client)gin.HandlerFunc{
+	return func(c *gin.Context){
+		tokenString,err:=c.Cookie("access_token")
 
+		if err!=nil{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"Unauthorized"})
+			return 
+		}
+
+
+		secret:=os.Getenv("JWT_SECRET")
+
+
+		token,err:=jwt.Parse(tokenString,func(token *jwt.Token)(interface{},error){
+			return []byte(secret),nil
+		})
+
+		if err!=nil{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"Invalid token"});
+			return 
+		}
+
+		claims:=token.Claims.(jwt.MapClaims)
+
+		userId,_:=bson.ObjectIDFromHex(claims["user_id"].(string))
+
+
+		
+	}
+}
